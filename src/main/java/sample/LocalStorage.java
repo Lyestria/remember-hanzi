c51fa43da5dd49e772b8735bbc7462da85086cb6
Kevin Wan <kevin.wan59049@gmail.com>
2021-01-15 18:37:04 -0500

    Change save directory to be under user home

--- src/main/java/sample/LocalStorage.java
+++ src/main/java/sample/LocalStorage.java
@@ -1,16 +1,23 @@
 package sample;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class LocalStorage {
 
-    private final Path filepath = Paths.get("saves","words.sav");
-    private final Path curDir = Paths.get(System.getProperty("user.dir"));
+
+    private final Path curDir = Paths.get(System.getProperty("user.home"));
     private final Path dir = Paths.get(curDir.toString(), "saves");
+    private final Path filepath = Paths.get(dir.toString(),"words.sav");
 
     public void saveData(WordBank bank)throws IOException{
+        if(Files.exists(dir)){
+            if(new File(dir.toString()).mkdirs()){
+                System.out.printf("Directory %s Created\n",dir.toString());
+            }
+        }
         try{
             FileOutputStream fileOut = new FileOutputStream(filepath.toString());
             ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
@@ -26,6 +33,7 @@ public class LocalStorage {
 
     public WordBank retrieveData() throws Exception{
         try {
+            System.out.println(filepath.toString());
             FileInputStream fileIn = new FileInputStream(filepath.toString());
             ObjectInputStream objIn = new ObjectInputStream(fileIn);
             WordBank bank = (WordBank)objIn.readObject();
