9194b119ac593a35c68e726b0e0e54cf280c6d24
Kevin Wan <kevin.wan59049@gmail.com>
2021-01-15 03:15:49 -0500

    Use OS independent pathing for files

--- src/main/java/sample/LocalStorage.java
+++ src/main/java/sample/LocalStorage.java
@@ -1,15 +1,18 @@
 package sample;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class LocalStorage {
 
-    private final String filepath = "saves/words.sav";
-    private final String dir = "saves/";
+    private final Path filepath = Paths.get("saves","words.sav");
+    private final Path curDir = Paths.get(System.getProperty("user.dir"));
+    private final Path dir = Paths.get(curDir.toString(), "saves");
 
     public void saveData(WordBank bank)throws IOException{
         try{
-            FileOutputStream fileOut = new FileOutputStream(filepath);
+            FileOutputStream fileOut = new FileOutputStream(filepath.toString());
             ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
             objOut.writeObject(bank);
             objOut.close();
@@ -23,14 +26,15 @@ public class LocalStorage {
 
     public WordBank retrieveData() throws Exception{
         try {
-            FileInputStream fileIn = new FileInputStream(filepath);
+            FileInputStream fileIn = new FileInputStream(filepath.toString());
             ObjectInputStream objIn = new ObjectInputStream(fileIn);
             WordBank bank = (WordBank)objIn.readObject();
             objIn.close();
             System.out.println("File Loaded!");
             return bank;
         } catch (Exception e){
-            File f = new File(System.getProperty("user.dir")+dir);
+            File f = new File(dir.toString());
+            System.out.println(dir.toString());
             System.out.println(f.mkdir());
             throw e;
         }
